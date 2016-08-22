33834a96afd587eaf2b2ecc765bdf07b23463e16
Katie <Katiejen81@gmail.com>
2016-08-21 21:33:26 -0500

    initial commit of Game Files

---
+++ Amelia_Math_Game.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Amelia's Math Program
+Created on Sun Aug 21 17:46:24 2016
+
+@author: Katie
+"""
+
+import pygame
+
+#Define Colors
+aqua = 0, 245, 255
+purple = 128, 0, 128
+black = 0, 0, 0
+white = 255, 255, 255
+
+#Set the screen size
+width = 800
+height = 600
+screen = pygame.display.set_mode(width, height)
+
+pygame.display.set_caption("Amelia's Math Game")
+
+#Initialize the Close Game Button
+
+done = False
---
+++ Math_game_level_1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Random Addition Problems - Level 1
+Created on Sun Aug 21 20:00:51 2016
+
+@author: Katie
+"""
+from __future__ import division   
+import random     
+
+selected = []
+while len(selected) < 5:
+    r = random.randint(0, 9)
+    if r in selected:
+        print r
+    else:
+        selected.append(r)
+        
+ccount = 0
+loopct = 0      
+for i in selected:
+    loopct = loopct + 1
+    j = random.randint(0, 1)
+    eq = ("%s " + "+" + " %s") % (i, j)
+    panswer = raw_input(eq)
+    canswer = i + j
+    if int(panswer) == canswer:
+        print "That's right!"
+        ccount = ccount + 1
+    elif int(panswer) != canswer and loopct < 5:
+        print "That's not right, let's try another one"
+    else:
+        print "That's not right, let's see how you did"
+        
+pctc = str((ccount / 5)*100) + "%"
+print "You got %d correct out of 5" % (ccount)
+print "That's %s correct!" % (pctc)      
+    
